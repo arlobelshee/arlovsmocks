32e896bdcebb293a8c5411f813e74b318d150823
Arlo Belshee <arlo.belshee@live.com>
2017-02-11 04:20:41 -0800

    Keep track of the error message in the critique.

--- ArloVsMocks/Critique.cs
+++ ArloVsMocks/Critique.cs
@@ -12,10 +12,17 @@ namespace ArloVsMocks
 			IsValid = isValid;
 		}
 
+		private Critique(int movieId, int criticId, int stars, bool isValid, string errorMessage)
+			: this(movieId, criticId, stars, isValid)
+		{
+			ErrorMessage = errorMessage;
+		}
+
 		public int MovieId { get; }
 		public int CriticId { get; }
 		public int Stars { get; }
 		public bool IsValid { get; }
+		public string ErrorMessage { get; }
 
 		public static Critique FromArgs(string[] args)
 		{
@@ -25,15 +32,15 @@ namespace ArloVsMocks
 			Critique critique;
 			try
 			{
-				movieId = Int32.Parse(args[0]);
-				criticId = Int32.Parse(args[1]);
-				stars = Int32.Parse(args[2]);
+				movieId = int.Parse(args[0]);
+				criticId = int.Parse(args[1]);
+				stars = int.Parse(args[2]);
 				critique = new Critique(movieId, criticId, stars, true);
 			}
 			catch (Exception ex)
 			{
 				Console.WriteLine(ex.Message);
-				critique = new Critique(0, 0, 0, false);
+				critique = new Critique(0, 0, 0, false, ex.Message);
 			}
 			return critique;
 		}
