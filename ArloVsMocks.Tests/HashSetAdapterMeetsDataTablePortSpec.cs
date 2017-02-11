df7be3a459be84150ef5bdee878c554780776852
Arlo Belshee <arlo.belshee@live.com>
2017-02-11 05:37:50 -0800

    Start defining the spec for the data table port. Save for valid data is pretty obvious.

---
+++ ArloVsMocks.Tests/HashSetAdapterMeetsDataTablePortSpec.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using ArloVsMocks.Data;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace ArloVsMocks.Tests
+{
+	[TestFixture]
+	public class HashSetAdapterMeetsDataTablePortSpec
+	{
+		protected virtual DataTablePort CreateTestSubject()
+		{
+			return new HashSet<Rating>().AsDataTablePort();
+		}
+
+		[Test]
+		public void SavingItemShouldAddItToExistingDataImmediately()
+		{
+			var testSubject = CreateTestSubject();
+			testSubject.ExistingData.Should().BeEmpty();
+			var newItem = new Rating
+			{
+				CriticId = 1,
+				MovieId = 2,
+				Stars = 3
+			};
+			testSubject.Save(newItem);
+			testSubject.ExistingData.Should().BeEquivalentTo(newItem);
+		}
+	}
+}
