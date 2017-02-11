f50dc30acc8e0ea7fbd4216562b173de874f7a8c
Arlo Belshee <arlo.belshee@live.com>
2017-02-11 05:46:46 -0800

    Verify that the code updates an existing matching rating in place.

--- ArloVsMocks.Tests/TrackRating.cs
+++ ArloVsMocks.Tests/TrackRating.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using ArloVsMocks.Data;
 using FluentAssertions;
 using NUnit.Framework;
@@ -9,6 +8,31 @@ namespace ArloVsMocks.Tests
 	[TestFixture]
 	public class TrackRating
 	{
+		[Test]
+		public void ExistingMatchingRatingShouldBeUpdated()
+		{
+			var data = new HashSet<Rating>();
+			var port = data.AsDataTablePort();
+			var critique = new Critique(1, 2, 3);
+			port.Save(new Rating
+			{
+				CriticId = critique.CriticId,
+				MovieId = critique.MovieId,
+				Stars = 1
+			});
+
+			var createdRating = new Rating
+			{
+				CriticId = critique.CriticId,
+				MovieId = critique.MovieId,
+				Stars = critique.Stars
+			};
+
+			Program.UpsertRating(port, critique);
+			data.Should()
+				.BeEquivalentTo(createdRating);
+		}
+
 		[Test]
 		public void NewRatingShouldBeCreated()
 		{
