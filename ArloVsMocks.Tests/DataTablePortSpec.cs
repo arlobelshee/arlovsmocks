0ba323621494a4e5638600a72cc6a2779983e60d
Arlo Belshee <arlo.belshee@live.com>
2017-02-11 06:00:10 -0800

    Split the port spec from the choice of which adapter to verify.

---
+++ ArloVsMocks.Tests/DataTablePortSpec.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using ArloVsMocks.Data;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace ArloVsMocks.Tests
+{
+	public abstract class DataTablePortSpec
+	{
+		protected abstract DataTablePort CreateTestSubject();
+
+		[Test]
+		public void SavingItemShouldAddItToExistingDataImmediately()
+		{
+			var testSubject = CreateTestSubject();
+			testSubject.ExistingData.Should().BeEmpty();
+			var newItem = new Rating
+			{
+				CriticId = 1,
+				MovieId = 2,
+				Stars = 3
+			};
+			testSubject.Save(newItem);
+			testSubject.ExistingData.Should().BeEquivalentTo(newItem);
+		}
+
+		[Test]
+		public void LoadingAnItemAndModifyingItShouldUpdateStoredItem()
+		{
+			var testSubject = CreateTestSubject();
+			var newItem = new Rating
+			{
+				CriticId = 1,
+				MovieId = 2,
+				Stars = 3
+			};
+			testSubject.Save(newItem);
+			var selectedRating = testSubject.ExistingData.First();
+			selectedRating.Stars = 5;
+			testSubject.ExistingData.Should().BeEquivalentTo(selectedRating);
+		}
+	}
+}
--- ArloVsMocks.Tests/HashSetAdapterMeetsDataTablePortSpec.cs
+++ ArloVsMocks.Tests/HashSetAdapterMeetsDataTablePortSpec.cs
@@ -1,48 +1,15 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using ArloVsMocks.Data;
-using FluentAssertions;
 using NUnit.Framework;
 
 namespace ArloVsMocks.Tests
 {
 	[TestFixture]
-	public class HashSetAdapterMeetsDataTablePortSpec
+	public class HashSetAdapterMeetsDataTablePortSpec : DataTablePortSpec
 	{
-		protected virtual DataTablePort CreateTestSubject()
+		protected override DataTablePort CreateTestSubject()
 		{
 			return new HashSet<Rating>().AsDataTablePort();
-		}
-
-		[Test]
-		public void SavingItemShouldAddItToExistingDataImmediately()
-		{
-			var testSubject = CreateTestSubject();
-			testSubject.ExistingData.Should().BeEmpty();
-			var newItem = new Rating
-			{
-				CriticId = 1,
-				MovieId = 2,
-				Stars = 3
-			};
-			testSubject.Save(newItem);
-			testSubject.ExistingData.Should().BeEquivalentTo(newItem);
-		}
-
-		[Test]
-		public void LoadingAnItemAndModifyingItShouldUpdateStoredItem()
-		{
-			var testSubject = CreateTestSubject();
-			var newItem = new Rating
-			{
-				CriticId = 1,
-				MovieId = 2,
-				Stars = 3
-			};
-			testSubject.Save(newItem);
-			var selectedRating = testSubject.ExistingData.First();
-			selectedRating.Stars = 5;
-			testSubject.ExistingData.Should().BeEquivalentTo(selectedRating);
 		}
 	}
 }
