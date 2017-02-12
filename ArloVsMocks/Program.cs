56cacaa6bcd43c5c35fc126e8aeba9701bb76952
Arlo Belshee <arlo.belshee@live.com>
2017-02-11 21:38:46 -0800

    Remove unused parameter. At this point the helper method is independent of the DB.

--- ArloVsMocks/Program.cs
+++ ArloVsMocks/Program.cs
@@ -23,7 +23,7 @@ namespace ArloVsMocks
 				var ratings = db.Ratings.ToDataTablePort(db);
 
 				UpsertRating(ratings, critique);
-				UpdateCriticRatingWeightAccordingToHowSimilarTheyAreToAverage(db, db.Critics.ToDataTablePort(db));
+				UpdateCriticRatingWeightAccordingToHowSimilarTheyAreToAverage(db.Critics.ToDataTablePort(db));
 
 				//re-calculate weighted average of all movie ratings
 				foreach (var movie in db.Movies)
@@ -54,7 +54,7 @@ namespace ArloVsMocks
 			Console.ReadKey();
 		}
 
-		private static void UpdateCriticRatingWeightAccordingToHowSimilarTheyAreToAverage(MovieReviewEntities db, DataTablePort<Critic> critics)
+		private static void UpdateCriticRatingWeightAccordingToHowSimilarTheyAreToAverage(DataTablePort<Critic> critics)
 		{
 			var criticsHavingRated = critics.ExistingData.Where(c => c.Ratings.Count > 0);
 			foreach (var critic in criticsHavingRated)
