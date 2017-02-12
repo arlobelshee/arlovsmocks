277cf7386a26f910c51e807a3d09e2234338fa0f
Arlo Belshee <arlo.belshee@live.com>
2017-02-12 02:36:12 -0800

    Now with a full weighted mean.

--- ArloVsMocks.Tests/MaintainRatings/TrackMovieRating.cs
+++ ArloVsMocks.Tests/MaintainRatings/TrackMovieRating.cs
@@ -24,6 +24,18 @@ namespace ArloVsMocks.Tests.MaintainRatings
 			movie.AverageRating.Should().BeApproximately(3.0, 0.0000001);
 		}
 
+		[Test]
+		public void MovieWithMultipleRatingsShouldHaveAWeightedMean()
+		{
+			var movie = Movie.Create(4);
+			AddRating(movie, 0.33, 3);
+			AddRating(movie, 1.0, 5);
+			AddRating(movie, 0.15, 2);
+			AddRating(movie, 0.15, 2);
+			Program.UpdateAverageRatingForMovie(movie);
+			movie.AverageRating.Should().BeApproximately(4.0429447852, 0.0000001);
+		}
+
 		private static void AddRating(Movie movie, double criticTrustworthiness, int stars)
 		{
 			movie.Ratings.Add(new Rating
