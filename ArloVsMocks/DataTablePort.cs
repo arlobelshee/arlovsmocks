4e8b7972c6c3ede44fdc0634ad33f6baad6c85a2
Arlo Belshee <arlo.belshee@live.com>
2017-02-11 21:08:33 -0800

    RISKY: use the methods via the interface rather than the ones I got as args.

--- ArloVsMocks/DataTablePort.cs
+++ ArloVsMocks/DataTablePort.cs
@@ -7,6 +7,7 @@ namespace ArloVsMocks
 	public class DataTablePort<T> where T : class
 	{
 		private readonly Action _saveChanges;
+		private readonly DataTablePortAdapter<T> _adapter;
 		private readonly Action<T> _saveImpl;
 
 		public DataTablePort(IQueryable<T> existingData, Action<T> saveImpl, Action saveChanges, DataTablePortAdapter<T> adapter)
@@ -14,18 +15,19 @@ namespace ArloVsMocks
 			ExistingData = existingData;
 			_saveImpl = saveImpl;
 			_saveChanges = saveChanges;
+			_adapter = adapter;
 		}
 
 		public IQueryable<T> ExistingData { get; }
 
-		public void Save(T existingT)
+		public void Save(T item)
 		{
-			_saveImpl(existingT);
+			_adapter.SaveItem(item);
 		}
 
 		public void PersistAll()
 		{
-			_saveChanges();
+			_adapter.PersistAll();
 		}
 	}
 }
